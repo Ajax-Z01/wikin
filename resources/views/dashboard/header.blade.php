52fcc29e47d681066b4caf07301700f4f4b167af
hammamhanif <hammam123ahmad@gmail.com>
2023-07-17 15:18:46 +0700

    perubahan dashboard admin

---
+++ resources/views/dashboard/header.blade.php
@@ -0,0 +1,148 @@
+<!-- Header Dashboard -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+
+    <div class="d-flex align-items-center justify-content-between">
+        <a href="index.html" class="logo d-flex align-items-center">
+            <img src="assets/img/logo.png" alt="">
+            <span class="d-none d-lg-block text-light">WIKIN</span>
+        </a>
+        <i class="bi bi-list text-light toggle-sidebar-btn"></i>
+    </div><!-- End Logo -->
+
+    <nav class="header-nav ms-auto">
+        <ul class="d-flex  align-items-center">
+
+            <li class="nav-item dropdown">
+
+                <a class="nav-link nav-icon " href="#" data-bs-toggle="dropdown">
+                    <i class="bi text-light bi-bell"></i>
+                    <span class="badge bg-primary text-light badge-number">1</span>
+                </a><!-- End Notification Icon -->
+
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+                    <li class="dropdown-header">
+                        You have a new notifications
+                        <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li class="notification-item">
+                        <i class="bi bi-exclamation-circle text-warning"></i>
+                        <div>
+                            <h4>Pengajuan Konsultasi</h4>
+                            <p>Pengadaaan survey didesa A</p>
+                            <p>4 hrs. ago</p>
+                        </div>
+                    </li>
+
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li class="dropdown-footer">
+                        <a href="#">Show all notifications</a>
+                    </li>
+
+                </ul><!-- End Notification Dropdown Items -->
+
+            </li><!-- End Notification Nav -->
+
+            <li class="nav-item dropdown">
+
+                <a class="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+                    <i class="bi text-light bi-chat-left-text"></i>
+                    <span class="badge bg-primary text-light badge-number">1</span>
+                </a><!-- End Messages Icon -->
+
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+                    <li class="dropdown-header">
+                        You have a new messages
+                        <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li class="message-item">
+                        <a href="#">
+                            <img src="assets/img/minato.jpg" alt="" class="rounded-circle">
+                            <div>
+                                <h4>Minato</h4>
+                                <p>Kerjaan hari ini bagaimana?, apakah...</p>
+                                <p>1 hrs. ago</p>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li class="dropdown-footer">
+                        <a href="#">Show all messages</a>
+                    </li>
+
+                </ul><!-- End Messages Dropdown Items -->
+
+            </li><!-- End Messages Nav -->
+
+            <li class="nav-item dropdown pe-3">
+
+                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                    <img src="{{ asset('img/naruto.jpg') }}" alt="Profile" class="rounded-circle">
+                    <span class="d-none text-light d-md-block dropdown-toggle ps-2">Arfanny</span>
+                </a><!-- End Profile Iamge Icon -->
+
+                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                    <li class="dropdown-header">
+                        <h6>Arfanny Yusuf H</h6>
+                        <span>Admin Wikin</span>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                            <i class="bi bi-person"></i>
+                            <span>My Profile</span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                            <i class="bi bi-gear"></i>
+                            <span>Account Settings</span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                            <i class="bi bi-question-circle"></i>
+                            <span>Need Help?</span>
+                        </a>
+                    </li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+
+                    <li>
+                        <a class="dropdown-item d-flex align-items-center" href="#">
+                            <i class="bi bi-box-arrow-right"></i>
+                            <span>Sign Out</span>
+                        </a>
+                    </li>
+
+                </ul><!-- End Profile Dropdown Items -->
+            </li><!-- End Profile Nav -->
+
+        </ul>
+    </nav><!-- End Icons Navigation -->
+
+</header>
---
+++ resources/views/dashboard/sidebar.blade.php
@@ -0,0 +1,92 @@
+<aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+        <li class="nav-item">
+            <a class="nav-link " href="index.html">
+                <i class="bi bi-grid"></i>
+                <span>Dashboard</span>
+            </a>
+        </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-menu-button-wide"></i><span>Menu Kelola</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="components-alerts.html" class="text-light">
+                        <i class="bi bi-circle"></i><span>User Terdaftar</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="components-accordion.html" class="text-light">
+                        <i class="bi bi-circle"></i><span>Landing Page</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="components-badges.html" class="text-light">
+                        <i class="bi bi-circle"></i><span>Profil Komunitas</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Components Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#forms-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-journal-text"></i><span>Form Pengajuan</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="forms-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="forms-elements.html" class="text-light">
+                        <i class="bi text-light bi-circle"></i><span>Pengabdian Masyarakat</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="forms-layouts.html" class="text-light">
+                        <i class="bi text-light bi-circle"></i><span>Komunitas</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Forms Pengajuan -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#keluhan-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-journal-text"></i><span>Form Keluhan</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="keluhan-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="forms-editors.html" class="text-light">
+                        <i class="bi text-light bi-circle"></i><span>Keluhan Kenukliran</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Forms Keluhan -->
+
+
+        <li class="nav-heading">More Info</li>
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="users-profile.html">
+                <i class="bi bi-person"></i>
+                <span>Profile</span>
+            </a>
+        </li><!-- End Profile Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-faq.html">
+                <i class="bi bi-question-circle"></i>
+                <span>Informasi</span>
+            </a>
+        </li><!-- End F.A.Q Page Nav -->
+
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="pages-contact.html">
+                <i class="bi bi-envelope"></i>
+                <span>Contact</span>
+            </a>
+        </li><!-- End Contact Page Nav -->
+
+
+    </ul>
+
+</aside>
